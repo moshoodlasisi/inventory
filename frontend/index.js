59269918d188741645715dc67cc5d802cf98fdc1
sikirumoshood <sikirumoshood@gmail.com>
2023-05-21 18:43:31 +0100

    Code fix

--- frontend/index.js
+++ frontend/index.js
@@ -2,30 +2,37 @@ const form = document.getElementById('login-form');
 const error = document.getElementById('error-message');
 const loginBtn = document.getElementById('login-btn');
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const username = form.username.value;
-        const password = form.password.value;
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = form.username.value;
+    const password = form.password.value;
 
-        fetch('/login', {
+    console.log(':::::: FORM DATA ::::::::', {username, password});
+
+    try{
+        const promiseResponse = await fetch('http://localhost:8000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-				Authentication: 'Bearer Token'
             },
+    
             body: JSON.stringify({
                 username,
                 password
             })
-            })
-        .then(response => response.json())
-        .then(data => {
-            if (data.accessToken) {
-                localStorage.setItem('accessToken', data.accessToken);
-                window.location.href = 'inventory.html';
-            } else {
-                error.style.display = 'block';
-            }
-        })
-        .catch(error => console.log(error));
-    });
+        });
+    
+        const data = await promiseResponse.json();
+    
+        if (data.accessToken) {
+            localStorage.setItem('loginData',  JSON.stringify({username, accessToken: data.accessToken}));
+            window.location.href = 'inventory.html';
+        } else {
+            alert('Error: No token returned')
+            error.style.display = 'block';
+        }
+        
+    }catch(error){
+        console.log('::: ERROR OCCURED', error)
+    }
+});
---
+++ frontend/inventory.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ * TODO:
+ * 1. Set the welcome message
+ * 2. Load all the inventories for the logged in user
+ * 
+ */
+
+const getLoginData = () => {
+  const loginDataString = localStorage.getItem('loginData');
+  return JSON.parse(loginDataString)
+}
+
+const setWelcomeText = () => {
+  const parentNode = document.getElementById('welcome-text');
+  const loginData = getLoginData();
+  parentNode.innerHTML = `<b>Welcome ${loginData.username}</b>`
+}
+
+const populateInventoriesTable = (inventories) => {
+  const tableBody = document.getElementById('inventory-table');
+  inventories.forEach((inventory) => {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${inventory.id}</td>
+      <td>${inventory.name}</td>
+      <td>${inventory.quantity}</td>
+    `;
+
+    tableBody.appendChild(row);
+  });
+}
+
+const getUserInventories = async () => {
+  try{
+
+    const loginData = getLoginData()
+    
+    const promiseResponse = await fetch('http://localhost:8000/inventory', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${loginData.accessToken}`
+      },
+    });
+
+    const inventories = await promiseResponse.json();
+
+    populateInventoriesTable(inventories)
+
+  }catch(error){
+    alert('Error: ' + error.message)
+    console.log('Failed to fetch inventories', error.message)
+  }
+}
+
+
+
+function main() {
+  setWelcomeText()
+  getUserInventories()
+}
+
+// Run script
+main()
